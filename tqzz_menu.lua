89e78465e78db8fd1fb0a2b80ad4476478596e89
Abde <31757534+lilynas@users.noreply.github.com>
2020-06-30 16:05:18 +0800

    添加目录

---
+++ tqzz_menu.lua
@@ -0,0 +1,33 @@
+local log = require("log")
+local Api = require("coreApi")
+local json = require("json")
+local http = require("http")
+
+function ReceiveFriendMsg(CurrentQQ, data)
+if string.find(data.Content, "天气之子") == 1 then
+		menu =  "如何使用天气之子？\n"..
+						"1.雷达图查询：华北、华中、华南、西北、西南、东北、东南雷达图\n"..
+						"2.气象云图\n"..
+                        "2.1:太平洋真彩色\n"..
+                        "2.2:太平洋红外图\n"..
+                        "2.3:中国真彩色\n"..
+						"3.中国降水图\n"..
+						"4.待添加\n"..
+					luaMsg =
+					    Api.Api_SendMsg(--调用发消息的接口
+					    CurrentQQ,
+					    {
+					       toUser = data.FromUin, --回复当前消息的来源群ID
+					       sendToType = 1, --2发送给群1发送给好友3私聊
+					       sendMsgType = "TextMsg", --进行文本复读回复
+					       groupid = 0, --不是私聊自然就为0咯
+					       content = menu, --回复内容
+					       atUser = 0 --是否 填上data.FromUserId就可以复读给他并@了
+					    }
+					)
+		end
+    return 1
+end
+function ReceiveGroupMsg(CurrentQQ, data)
+    return 1
+end
